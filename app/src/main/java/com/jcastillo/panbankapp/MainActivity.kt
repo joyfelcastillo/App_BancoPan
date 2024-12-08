58125c40a1512c74fe36ad5650754021110c4657
joyfel <joyfelcastilloc@gmail.com>
2024-12-08 16:28:40 -0300

    nuevos cambios

--- app/src/main/java/com/jcastillo/panbankapp/MainActivity.kt
+++ app/src/main/java/com/jcastillo/panbankapp/MainActivity.kt
@@ -2,12 +2,16 @@ package com.jcastillo.panbankapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.card.MaterialCardView
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
